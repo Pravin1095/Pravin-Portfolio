efdc596a8507ac99f31403d13fb47f68c1977b3f
Pravin A <a.pravin3210@gmail.com>
2022-12-24 12:37:39 +0530

    #imported Script in _app.js to add bootstrap.js cdn

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,18 @@
 import '../styles/globals.css'
-import 'bootstrap/dist/css/bootstrap.css'; 
-import "bootstrap/dist/js/bootstrap.js"
+import 'bootstrap/dist/css/bootstrap.css';
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      
+        <Script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></Script>
+        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></Script>
+      
+
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp

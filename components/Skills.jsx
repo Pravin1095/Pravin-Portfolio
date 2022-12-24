69c904ed1305264d0ef74d4b7b1cbfd720328b48
Pravin A <a.pravin3210@gmail.com>
2022-12-24 21:35:08 +0530

    #added Fontawesome and added responsive grid layout to skills component

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,27 +1,37 @@
 import React from "react";
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+// import your icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCloud } from '@fortawesome/free-solid-svg-icons';
+import { faHtml5,faCss3Alt,faJs,faReact,faPython,faJava,faClo} from '@fortawesome/free-brands-svg-icons';
 
 function Skills() {
   return (
     <div className="skill">
       <h2 className="skills-h2">My Skills</h2>
-
-      <div className="total-skills">
+      <div className='row total-skills'>
+      <div className="col-lg-4">
         <h3>FrontEnd Technologies</h3>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JavaScript</li>
-          <li>React</li>
-        </ul>
+        
+        <div className='pad-style'><FontAwesomeIcon icon={faHtml5}></FontAwesomeIcon><h5>HTML</h5></div>
+        <div className='pad-style'><FontAwesomeIcon icon={faCss3Alt}></FontAwesomeIcon><h5>CSS</h5></div>
+        <div className='pad-style'><FontAwesomeIcon icon={faJs}></FontAwesomeIcon><h5>JavaScript</h5></div>
+        <div className='pad-style'><FontAwesomeIcon icon={faReact}></FontAwesomeIcon><h5>React</h5></div>
+        
+        
       </div>
 
-      <div className="total-skills">
+      <div className="col-lg-4">
         <h3>BackEnd Technologies</h3>
-        <ul>
-          <li>Python</li>
-          <li>MongoDB</li>
-          <li>Java</li>
-        </ul>
+        <div className='pad-style'><FontAwesomeIcon icon={faPython}></FontAwesomeIcon><h5>Python</h5></div>
+        <div className='pad-style'><FontAwesomeIcon icon={faJava}></FontAwesomeIcon><h5>Java Basics</h5></div>
+        
+      </div>
+      <div className='col-lg-4'>
+      <h3>Other Technologies</h3>
+        <div className='pad-style'><FontAwesomeIcon icon={faCloud}></FontAwesomeIcon><h5>Google Cloud Basics</h5></div>
+      </div>
       </div>
     </div>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
-
+import 'bootstrap/dist/css/bootstrap.css';
 import Head from 'next/head'
 import Script from 'next/script'
 
@@ -9,6 +9,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
       </Head>
+      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></Script>
         <Script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></Script>
         <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></Script>
       

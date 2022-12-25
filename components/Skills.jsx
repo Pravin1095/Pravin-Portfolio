1bdd03b4456f3f636b49d344191c56f809356a94
Pravin A <a.pravin3210@gmail.com>
2022-12-25 20:58:39 +0530

    #Reduced FontAwesomes width

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -15,10 +15,10 @@ function Skills() {
       <div className="col-lg-4">
         <h3>FrontEnd Technologies</h3>
         
-        <div className='pad-style'><FontAwesomeIcon icon={faHtml5}></FontAwesomeIcon><h5>HTML</h5></div>
-        <div className='pad-style'><FontAwesomeIcon icon={faCss3Alt}></FontAwesomeIcon><h5>CSS</h5></div>
-        <div className='pad-style'><FontAwesomeIcon icon={faJs}></FontAwesomeIcon><h5>JavaScript</h5></div>
-        <div className='pad-style'><FontAwesomeIcon icon={faReact}></FontAwesomeIcon><h5>React</h5></div>
+        <div className='pad-style'><FontAwesomeIcon icon={faHtml5} style={{width:'50px'}}></FontAwesomeIcon><h5>HTML</h5></div>
+        <div className='pad-style'><FontAwesomeIcon icon={faCss3Alt} style={{width:'50px'}}></FontAwesomeIcon><h5>CSS</h5></div>
+        <div className='pad-style'><FontAwesomeIcon icon={faJs} style={{width:'50px'}}></FontAwesomeIcon><h5>JavaScript</h5></div>
+        <div className='pad-style'><FontAwesomeIcon icon={faReact} style={{width:'50px'}}></FontAwesomeIcon><h5>React</h5></div>
         
         
       </div>
@@ -26,12 +26,12 @@ function Skills() {
       <div className="col-lg-4">
         <h3>BackEnd Technologies</h3>
         <div className='pad-style'><FontAwesomeIcon icon={faPython} style={{width:'50px'}}></FontAwesomeIcon><h5>Python</h5></div>
-        <div className='pad-style'><FontAwesomeIcon icon={faJava}></FontAwesomeIcon><h5>Java Basics</h5></div>
+        <div className='pad-style'><FontAwesomeIcon icon={faJava} style={{width:'50px'}}></FontAwesomeIcon><h5>Java Basics</h5></div>
         
       </div>
       <div className='col-lg-4'>
       <h3>Other Technologies</h3>
-        <div className='pad-style'><FontAwesomeIcon icon={faCloud}></FontAwesomeIcon><h5>Google Cloud Basics</h5></div>
+        <div className='pad-style'><FontAwesomeIcon icon={faCloud} style={{width:'50px'}}></FontAwesomeIcon><h5>Google Cloud Basics</h5></div>
       </div>
       </div>
       </div>

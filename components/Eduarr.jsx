20d3513ce2476864fb8ae11ebd2fa225044c545f
Pravin A <a.pravin3210@gmail.com>
2023-01-04 21:58:48 +0530

    #added edu array objects

---
+++ components/Eduarr.jsx
@@ -0,0 +1,17 @@
+edu=[
+    {
+        link:'https://www.salemholycross.com/',
+        institute:'Holy Cross Matriculation Higher Secondary School',
+        school:'-School',
+        year:'2006-2018'
+    },
+    {
+        link:'https://www.sastra.edu/',
+        institute:'SASTRA Deemed To Be University',
+        school:'-B.Tech Mechanical Engineering',
+        year:'2018-2022'
+    }
+
+]
+
+export default edu

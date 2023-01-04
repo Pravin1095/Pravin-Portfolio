d1ed574d6a2e7af6f3b0fd8a82dc4f23196de860
Pravin A <a.pravin3210@gmail.com>
2023-01-04 22:02:21 +0530

    #Renamed Contact2 to Footer

---
+++ components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer=()=>{
+    return(
+        <section id='contact2'>
+<div class="bottom-container container-fluid">
+<div>
+     <a className='footer-link' href="">LinkedIn</a>
+     <a className='footer-link' href="">Twitter</a>
+     </div>
+     <p className="copyright">© Pravin A</p>
+     
+ </div>
+ </section>
+    )
+}
+
+export default Footer
+

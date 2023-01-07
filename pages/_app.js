41e9edba6553958493fb90e0ddb22a30d7849015
Pravin A <a.pravin3210@gmail.com>
2023-01-07 22:34:07 +0530

    #import bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 
-// import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
 import { useEffect } from "react";
 import bootstrap from 'bootstrap'
 import Head from 'next/head'
@@ -11,7 +12,10 @@ function MyApp({ Component, pageProps }) {
 
   // useEffect(() => {
   //  require("bootstrap/dist/js/bootstrap.bundle.min.js");
-  //}, []); 
+  // }, []); 
+  useEffect(() => {
+    import('bootstrap')
+  }, [])
 
   return (
     <div>

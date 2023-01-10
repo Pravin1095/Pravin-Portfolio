52fdd0a194162a0f4dd0106067db897fd6d31969
Pravin A <a.pravin3210@gmail.com>
2023-01-10 18:46:13 +0530

    #sending props to skills

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,40 +1,32 @@
 import React from "react";
-import { library } from '@fortawesome/fontawesome-svg-core';
-
 // import your icons
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCloud } from '@fortawesome/free-solid-svg-icons';
-import { faHtml5,faCss3Alt,faJs,faReact,faPython,faJava,faClo} from '@fortawesome/free-brands-svg-icons';
+import {skillarr1,skillarr2,skillarr3} from './skillarr'
 
-const Skills=()=> {
+
+const SkillList = ({title,skillarray}) => {
+  return <div className="col-lg-4">
+    <h3>{title}</h3>
+    
+    <div>{skillarray.map((skill,index) => {
+      {skill}
+     return <div key={index} className='pad-style'><FontAwesomeIcon icon={skill.icon} style={{ width: '50px' }}></FontAwesomeIcon><h5>{skill.title}</h5></div>
+
+    })}</div>
+  </div>
+}
+const Skills = () => {
   return (
     <section id="skill">
-    <div className='container-fluid'>
-   
-      <h2 className="skills-h2">My Skills</h2>
-      <div className='row total-skills'>
-      <div className="col-lg-4">
-        <h3>FrontEnd Technologies</h3>
-        
-        <div className='pad-style'><FontAwesomeIcon icon={faHtml5} style={{width:'50px'}}></FontAwesomeIcon><h5>HTML</h5></div>
-        <div className='pad-style'><FontAwesomeIcon icon={faCss3Alt} style={{width:'50px'}}></FontAwesomeIcon><h5>CSS</h5></div>
-        <div className='pad-style'><FontAwesomeIcon icon={faJs} style={{width:'50px'}}></FontAwesomeIcon><h5>JavaScript</h5></div>
-        <div className='pad-style'><FontAwesomeIcon icon={faReact} style={{width:'50px'}}></FontAwesomeIcon><h5>React</h5></div>
-        
-        
-      </div>
+      <div className='container-fluid'>
 
-      <div className="col-lg-4">
-        <h3>BackEnd Technologies</h3>
-        <div className='pad-style'><FontAwesomeIcon icon={faPython} style={{width:'50px'}}></FontAwesomeIcon><h5>Python</h5></div>
-        <div className='pad-style'><FontAwesomeIcon icon={faJava} style={{width:'50px'}}></FontAwesomeIcon><h5>Java Basics</h5></div>
-        
-      </div>
-      <div className='col-lg-4'>
-      <h3>Other Technologies</h3>
-        <div className='pad-style'><FontAwesomeIcon icon={faCloud} style={{width:'50px'}}></FontAwesomeIcon><h5>Google Cloud Basics</h5></div>
-      </div>
-      </div>
+        <h2 className="skills-h2">My Skills</h2>
+        <div className='row total-skills'>
+        <SkillList title='FrontEnd Technologies' skillarray={skillarr1} />
+        <SkillList title='BackEnd Technologies' skillarray={skillarr2} />
+        <SkillList title='Other Technologies' skillarray={skillarr3} />
+         
+        </div>
       </div>
     </section>
   );

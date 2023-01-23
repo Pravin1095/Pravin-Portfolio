70eeec3fd2d895d0ea312793cc9f0ae928b19ac8
Pravin A <a.pravin3210@gmail.com>
2023-01-23 22:18:30 +0530

    #Image tag added

--- components/hobbyarr.js
+++ components/hobbyarr.js
@@ -1,13 +1,13 @@
 const harr=[
     {
-        imglink:'./images/chess_img.jpg',
+        imglink:'/images/chess_img.jpg',
         classType:'l-img',
         alter:'chess-img',
         title:'Chess Player',
         description:'I am a District Level Chess Player and participated in many tournaments and love to play and watch the game.'
     },
     {
-        imglink:'./images/fit_img.jpg',
+        imglink:'/images/fit_img.jpg',
         classType:'r-img',
         alter:'fitness-img',
         title:'Jogging and Exercising',

174b12de5b1a8f415090a0435e5f6ad3fae3468f
Pravin A <a.pravin3210@gmail.com>
2026-04-02 01:18:44 +0530

    Adding the index.js in pages folder

--- components/App.jsx
+++ components/App.jsx
@@ -1,29 +1,5 @@
-import React from "react";
-import MyHome from "../pages/MyHome.jsx";
-import NameParent from "./Name.jsx";
-import IntroParent from "./Intro.jsx";
-// import Skills from './Skills.jsx'
-import BCarousel from "./BCarousel.jsx";
-import Educations from './Education.jsx'
-// import ContactParent from './Contact.jsx'
-import FooterParent from './Footer.jsx'
-import Eduarr from './Eduarr.jsx'
+
 import Home from "./RedesignPortfolio/Home.jsx";
-
-import Example from "./OffCanvasNav.jsx";
-import Trainee from "./Trainee.jsx";
-// import Home from "./MyHome.jsx";
-import app_arr from "./apparr.js";
-import HobbyParent from "./Hobbies.jsx";
-import Project_Clg from "../pages/Project_Clg.jsx";
-import Project_Cs_Parent from "../pages/Project_Cs.jsx";
-import Project from "../pages/Project.jsx";
-
-import Router from "react-router-dom"
-import Switch from "react-router-dom"
-import Route from "react-router-dom"
-import Redirect from "react-router-dom"
-import RedesignPortfolio from "../pages/RedesignPortfolio.jsx";
 import Skills from "./RedesignPortfolio/Skills.js";
 import Contact from "./RedesignPortfolio/Contact.jsx";
 import About from "./RedesignPortfolio/About.jsx";
@@ -62,28 +38,6 @@ const handleScroll = (id) => {
     <About id="about" />
     <Skills id="skills" />
     <Contact id="contact" />
-    {/* <RedesignPortfolio /> */}
-    {/* <MyHome /> */}
-    {/* <Project /> */}
-   {/* <div>{app_arr.map(
-      (apps,index)=>
-      {
-        return <React.Fragment key={index}>
-       
-         {apps.child}
-       
-        </React.Fragment>
-      }
-    )}</div>  */}
-  {/* <Home />
-      
-      <IntroParent />
-      <ControlledCarousel />
-      
-      <Educations />
-      <Trainee />
-      <ContactParent />
-      <FooterParent /> */}
     </div>
   );
 }
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+// import Head from 'next/head'
+// import Image from 'next/image'
+// import styles from '../styles/Home.module.css'
+import App from '../components/App.jsx'
+
+
+function Home() {
+  
+  return (
+    <App />
+  )
+}
+
+export default Home
